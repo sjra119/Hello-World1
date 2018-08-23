068aaaf4873a27107c1f204bb1953c106241fae3
sj ra <raras1119@gmail.com>
2018-08-23 20:27:18 +0900

    첫 커밋

--- src/main/java/com/nshome/inflearn/intellij/Main.java
+++ src/main/java/com/nshome/inflearn/intellij/Main.java
@@ -2,6 +2,6 @@ package com.nshome.inflearn.intellij;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("aaa");
+        System.out.println("aa롷로ㅗㅎㅀㄹㅀㅎㄹa");
     }
 }
